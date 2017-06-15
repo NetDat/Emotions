c082097aa9736d9cf82fbeb0513892e000970bfa
netdatatech <maghsoudi@outlook.com>
2017-06-16 00:32:39 +1200

    Add project files.

---
+++ Tabs/CustomVision.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Tabs.Model;
+using Xamarin.Forms;
+using Tabs.Model.Tabs.Model;
+
+namespace Tabs
+{
+    public partial class CustomVision : ContentPage
+    {
+        public CustomVision()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoadCamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Sample",
+                Name = $"{DateTime.UtcNow}.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                return file.GetStream();
+            });
+
+
+            await MakePredictionRequest(file);
+        }
+
+        static byte[] GetImageAsByteArray(MediaFile file)
+        {
+            var stream = file.GetStream();
+            BinaryReader binaryReader = new BinaryReader(stream);
+            return binaryReader.ReadBytes((int)stream.Length);
+        }
+
+        async Task MakePredictionRequest(MediaFile file)
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Prediction-Key", "a9d47b83b3f54deda154a48b5ec8026c");
+
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/7d2044db-ca22-4145-a061-d86b4cb581de/image?iterationId=2edff9cb-e25c-4682-8f6f-b0b461e0f379";
+
+            HttpResponseMessage response;
+
+            byte[] byteData = GetImageAsByteArray(file);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    EvaluationModel responseModel = JsonConvert.DeserializeObject<EvaluationModel>(responseString);
+
+                    double max = responseModel.Predictions.Max(m => m.Probability);
+
+                    TagLabel.Text = (max >= 0.5) ? "Hotdog" : "Not Hotdog";
+
+                }
+
+                //Get rid of file once we have finished using it
+                file.Dispose();
+            }
+        }
+    }
+}
